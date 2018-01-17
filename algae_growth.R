f5555df792219e496c888de7a3291e69500b3fc6
Isabella Velásquez <isabella.velasquez@gatesfoundation.org>
2018-01-17 08:24:26 -0800

    more fiddling with the viz & addition of algae type

--- algae_growth.R
+++ algae_growth.R
@@ -11,6 +11,7 @@
 library(XML)
 library(tidyverse)
 library(ggplot2)
+library(ggthemes)
 
 
 # data --------------------------------------------------------------------
@@ -18,13 +19,21 @@ library(ggplot2)
 algae <- "http://aquatext.com/tables/algaegrwth.htm"
 
 algae_tab <-
-  readHTMLTable(algae, header = F, which = 2, stringsAsFactors = F)
+  readHTMLTable(algae, header = F, which = 2, stringsAsFactors = F, trim = T)
+
+algae_types <- # data downloaded from algaebase
+  read_csv("algae_types.csv")
 
 
 # tidy table --------------------------------------------------------------
 
-algae_tab_rename <-
-  algae_tab %>% 
+algae_tab_names <- # some species downloaded with tab separators
+  algae_tab %>%
+  mutate(V1 = if_else(V1 == "Chlorella vulgaris\n      (freshwater)", "Chlorella vulgaris (freshwater)", V1)) %>% 
+  mutate(V1 = if_else(V1 == "Nannochlorois\n      oculata", "Nannochlorois oculata", V1))
+
+algae_tab_tidy <-
+  algae_tab_names %>% 
   rename("species" = "V1", # this creates colnames that can be gathered
          "5.5000" = "V2",
          "5.2500" = "V3",
@@ -36,22 +45,33 @@ algae_tab_rename <-
          "30.2500" = "V9") %>% 
   slice(-1:-3) %>% # no longer needed rows
   gather("temp_light", "growth_rate", -1) %>%
-  mutate(temp = str_extract(temp_light, "\\d+"),
+  mutate(temp = paste0(str_extract(temp_light, "\\d+"), "°"),
          light = str_replace(temp_light,"\\d+.",""),
          growth_rate = as.numeric(growth_rate),
          growth_rate = if_else(species == "Isochrysis aff. galbana" & is.na(growth_rate), 0.06, growth_rate)) # this variable was missing
 
+algae_tab_type <-
+  left_join(algae_tab_tidy, algae_types)
+
 
 # visualization -----------------------------------------------------------
 
-algae_tab_rename$temp <- factor(algae_tab_rename$temp, levels = c("5", "10", "25", "30"))
+algae_tab_type$temp <- factor(algae_tab_type$temp, levels = c("5°", "10°", "25°", "30°"))
 
-algae_tab_rename %>%
-  ggplot(aes(y = growth_rate, x = light, fill = light)) +
-  geom_point() +
-  facet_grid(.~temp)
+algae_tab_type$salinity <- as.character(algae_tab_type$salinity)
+algae_tab_type$salinity <- factor(algae_tab_type$salinity, levels = c("35", "0"))
 
+algae_tab_type %>%
+  ggplot(aes(y = growth_rate, x = light, color = type)) +
+  geom_jitter(width = .1, size = 4) +
+  facet_grid(~temp) +
+  labs(x = "Light Intensity (lux)", y = "Growth Rate", title = "Specific growth rates of algae (divisions per day) at different light intensities and temperatures",  caption = "Source: Aquatext, Algaebase") + 
+  theme_economist_white(gray_bg=FALSE) +
+  scale_color_economist(name = "Algae Type") +
+  guides(color = guide_legend(override.aes = list(size = 3))) + 
+  theme(axis.title = element_text(size = 16),
+        axis.text = element_text(size = 12))
 
 # cleaned dataset ---------------------------------------------------------
 
-write.csv(algae_tab_rename, "algae_tab_rename.csv")
+write.csv(algae_tab_type, "./processed/algae_processed.csv")
